f3f59da639f801bf30ca1980363a568e9b128c39
Jahongir95 <sjahongir0032@gmail.com>
2020-12-23 11:31:09 +0500

    sidebar is creating

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 var hamburgerMenu = document.querySelector('#hamburger');
+var sidebar = document.querySelector('#sidebar');
 
 hamburgerMenu.addEventListener('click', function(){
     hamburgerMenu.classList.toggle('clicked');
+    sidebar.classList.toggle('opened');
+
 });
