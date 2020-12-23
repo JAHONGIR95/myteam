804cc3e96161f40da9d35cd37b929aa3aa194afc
Jahongir95 <sjahongir0032@gmail.com>
2020-12-23 14:12:59 +0500

    sidebar is added

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 var hamburgerMenu = document.querySelector('#hamburger');
+var hamburgerMenu2 = document.querySelector('#hamburger2');
 var sidebar = document.querySelector('#sidebar');
 var sidebarButton = document.querySelector('#sidebar-button');
 var body = document.querySelector('body');
@@ -6,6 +7,15 @@ var overlay = document.querySelector('#overlay');
 
 hamburgerMenu.addEventListener('click', function(){
     hamburgerMenu.classList.toggle('clicked');
+    sidebar.classList.toggle('opened');
+    sidebarButton.classList.toggle('opened');
+    overlay.classList.toggle('opened');
+    body.classList.toggle('opened');
+
+});
+
+hamburgerMenu2.addEventListener('click', function(){
+    hamburgerMenu2.classList.toggle('clicked');
     sidebar.classList.toggle('opened');
     sidebarButton.classList.toggle('opened');
     overlay.classList.toggle('opened');
