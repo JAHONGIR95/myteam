3f7c7745684b85b38a7fe8a007b403db8e7f3e0d
Jahongir95 <sjahongir0032@gmail.com>
2020-12-23 12:26:02 +0500

    functions are added to sidebar

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 var hamburgerMenu = document.querySelector('#hamburger');
 var sidebar = document.querySelector('#sidebar');
+var sidebarButton = document.querySelector('#sidebar-button');
+// var sidebarLink = document.querySelector('#sidebar-link');
+var body = document.querySelector('body');
 
 hamburgerMenu.addEventListener('click', function(){
     hamburgerMenu.classList.toggle('clicked');
     sidebar.classList.toggle('opened');
-
+    sidebarButton.classList.toggle('opened');
+    body.classList.toggle('opened');
+    // sidebarLink.classList.toggle('opened');
+// 
 });
