34236822654b1338646182e73f0cc1abfdac919d
Jahongir95 <sjahongir0032@gmail.com>
2020-12-23 12:41:34 +0500

    sidebar is finished

--- js/main.js
+++ js/main.js
@@ -2,13 +2,13 @@ var hamburgerMenu = document.querySelector('#hamburger');
 var sidebar = document.querySelector('#sidebar');
 var sidebarButton = document.querySelector('#sidebar-button');
 // var sidebarLink = document.querySelector('#sidebar-link');
-var body = document.querySelector('body');
+var overlay = document.querySelector('#overlay');
 
 hamburgerMenu.addEventListener('click', function(){
     hamburgerMenu.classList.toggle('clicked');
     sidebar.classList.toggle('opened');
     sidebarButton.classList.toggle('opened');
-    body.classList.toggle('opened');
+    overlay.classList.toggle('opened');
     // sidebarLink.classList.toggle('opened');
 // 
 });
