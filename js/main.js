ec8afc5125da2a742b4da43808b42464acc4e2f7
Jahongir95 <sjahongir0032@gmail.com>
2021-05-14 23:38:07 +0500

    my team project is updated

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 var hamburgerMenu = document.querySelector('#hamburger');
-// var hamburgerMenu2 = document.querySelector('#hamburger2');
 var sidebar = document.querySelector('#sidebar');
 var sidebarButton = document.querySelector('#sidebar-button');
 var body = document.querySelector('body');
@@ -11,14 +10,35 @@ hamburgerMenu.addEventListener('click', function(){
     sidebarButton.classList.toggle('opened');
     overlay.classList.toggle('opened');
     body.classList.toggle('opened');
-
+    
 });
 
-hamburgerMenu2.addEventListener('click', function(){
-    // hamburgerMenu2.classList.toggle('clicked');
-    sidebar.classList.toggle('opened');
-    sidebarButton.classList.toggle('opened');
-    overlay.classList.toggle('opened');
-    body.classList.toggle('opened');
 
-});
+const cards = document.querySelectorAll('.card');
+const cardButtons = document.querySelectorAll('.card__button')
+
+
+function removeAllClasses(clickedElement){
+
+    cards.forEach(card => {
+        if(card.dataset.cardOrder != clickedElement) {
+            card.classList.remove('active')
+        } else {
+            card.classList.toggle('active')
+        }
+    })
+}
+
+
+cardButtons.forEach(button => {
+    
+    button.addEventListener('click', function(){
+        localStorage.setItem('cardId', button.dataset.buttonOrder)
+        removeAllClasses(localStorage.getItem('cardId'))
+        
+    })
+})
+
+if(localStorage.getItem('cardId')){
+    removeAllClasses(localStorage.getItem('cardId'))
+} 
